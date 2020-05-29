5476d4a909d7c2e17c2b5c7c4f58823bfae9f447
sachitha1990 <sachitha.perera@summitaustralia.com.au>
2020-05-29 17:03:26 +0530

    elite_fam_user_reg intial commit

---
+++ user_create/user_reg/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import JoinRequest
+
+# Register your models here.
+admin.site.register(JoinRequest)
---
+++ user_create/user_reg/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class JoinRequest(models.Model):
+    TITLES =(
+        ('Mr', 'Mr'),
+        ('Ms', 'Ms')
+    )
+    STATUS =(
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('confirmed', 'Confirmed'),
+        ('rejected', 'Rejected')
+    )
+    PROVINCES =(
+        ('cp', 'Central Province'),
+        ('ep', 'Eastern Province'),
+        ('np', 'Northern Province'),
+        ('sp', 'Southern Province'),
+        ('wp', 'Western Province'),
+        ('nwp', 'North Western Province'),
+        ('ncp', 'North Central Province'),
+        ('up', 'Uva Province'),
+        ('sgp', 'Sabaragamuwa Province'),
+    )
+    title = models.CharField(null=False, choices=TITLES, max_length=36)
+    first_name = models.CharField(null=False, max_length=100)
+    middle_name = models.CharField(max_length=100)
+    last_name = models.CharField(null=False,max_length=100)
+    nic = models.CharField(null=False, max_length=12,default=None)
+
+    email = models.EmailField(null=False, default=None)
+    address_line_one = models.CharField(null=False,max_length=100, default=None)
+    address_line_two = models.CharField(null=False,max_length=100, default=None)
+    city = models.CharField(null=False,max_length=150,default=None)
+    province = models.CharField(null=False,choices=PROVINCES, max_length=50, default=None)
+
+    vehicle_reg = models.CharField(null=False, max_length=20,default=None)
+    photo = models.ImageField(default=None)
+
+    applied_date = models.DateField(auto_now=True)
+    status = models.CharField(null=False, default='pending', choices=STATUS, max_length=20)
+    is_published = models.BooleanField(default=False)
